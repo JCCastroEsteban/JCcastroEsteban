495bb6a97e69612b2e7e38d04bd08501f43f0bdb
JCCastroEsteban <camilo_grung@hotmail.com>
2024-02-09 13:08:05 -0500

    hola

---
+++ README.js
@@ -0,0 +1,11 @@
+<div>
+ <h1>Hola Soy Camilo 💻 👋</h1>
+ <h2>Colombia/Bogotá</h2>
+ <h3>Soy una persona a la que le gusta mucho la programación. Por eso mismo, decidí integrarme en el mundo del Desarrollo y aunque mi formación es 
+  como Full Stack, tengo una preferencia hacia el área de Front-End. Actualmente, continúo instruyéndome con nuevas 
+  tecnologías en ambas áreas para poder desarrollar aún más mis habilidades e incrementar mis conocimientos.
+</h3>
+</div> 
+
+  
+
